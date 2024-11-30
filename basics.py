3c02c0ae643adc256450379e45b878357803b110
Muhammad Qasim Sher Khan Sherwani <mqasim111786111@gmail.com>
2024-12-01 02:28:08 +0500

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,132 @@
+a = "Hello how are u?"
+#print(a)
+
+
+a = "Why are u fine?"
+print(a)
+
+#%%
+
+# Use a variable to represent a person’s name.
+# ● Print a message to that person, such as, “Hello Eric, would you like to learn some
+# Python today?”
+# 4. Name Cases
+# Task:
+# ● Use a variable to represent a person’s name.
+# ● Print the person’s name in lowercase, uppercase, and title case.
+# 5. Famous Quote
+# Task:
+# ● Find a quote from a famous person you admire.
+# ● Print the quote and the name of its author.
+# The output should look something like the following:
+# Albert Einstein once said, “A person who never made a mistake never
+# tried anything new.”
+
+a = "Eric"
+print(f" Hi {a.lower()} how are u")
+
+print(f" Hi {a.upper()} how are u")
+
+print(f" Hi {a.title()} how are u")
+
+#%%
+
+
+city = "Lahore"
+print(city)
+
+#%%
+
+temperature = 25
+
+print (f"The current temperature is {temperature} degrees.")
+
+
+#%%
+
+# Create a variable poem with a short poem that has multiple lines. Print the poem with
+# each line starting on a new line.
+
+
+
+poem = """
+          PIAIC was found out by Mr.Zia,\nTheir is a guy Umair khan, sir jahanzeb knows him well, with Mustaches in 3rd quarter\nHe works daya and night, completes the projects,  presented custom GPTS to Zia Sahab. \nBut Zia sahab never thinks of inducting him as an instructor at PIAIC, \nHe participated in Hackathons, made projects on LinkedIns, It is a request to sir Jahanzeb,\nconvey my msg to sir zia, plz consider my brother for the post of instructor at PIAIC.
+          
+
+        """
+
+print(poem)
+
+
+#%%
+
+score = 100
+print(score)
+
+#%%
+
+a = "PYTHON PROGRAMMING"
+
+
+print(a.title())
+
+
+#%%
+
+
+a = "PYTHON pROGRAMMING"
+
+print(a.lower())
+
+#%%
+
+a = "PYTHON pROGRAMMING"
+
+print(a.upper())
+
+
+#%%
+
+
+
+# Use a variable to represent a person’s name, and include some whitespace characters
+# at the beginning and end of the name.
+# ● Make sure you use each character combination, \t and \n, at least once.
+# ● Print the name once, so the whitespace around the name is displayed.
+# ● Print the name using each of the three stripping functions: lstrip(), rstrip(), and
+# strip().
+
+
+a = "   Muhammad   "
+
+print(a)
+print(a.strip())
+
+#%%
+
+x = "How"
+y = "you"
+
+print(x)
+print(y)
+
+
+temp = x
+x = y
+y = temp
+
+
+print(x)
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
